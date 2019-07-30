33acc44f63c6f07a5899d9ff0a7b2ec8228d62b8
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-30 16:13:59 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+RSA="\033[31m"
+YSA="\033[1;93m"
+CEA="\033[0m"
+WHS="\033[0;97m"
+
+WHO="$( whoami )"
+
+if [[ "$WHO" != "root" ]]
+then
+    echo -e ""$RSA"[-]"$WHS" [Errno 1] Failed to copy files: Operation not permitted"$CEA""
+    exit
+exit
+fi
+
+if [[ -d ~/entypreter ]]
+then
+sleep 0
+else
+cd ~
+{
+git clone https://github.com/entynetproject/entypreter.git
+} &> /dev/null
+cd  ~/entypreter
+fi
+
+{
+cd 
+cd entypreter
+cp bin/entypreter /usr/local/bin
+chmod +x /usr/local/bin/entypreter
+cp bin/entypreter /bin
+chmod +x /bin/entypreter
+} &> /dev/null
